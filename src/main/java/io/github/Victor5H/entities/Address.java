0513b4759e93b9ec167285b9e003d2af3fb575a6
harshit <harshitsathone@gmail.com>
2023-03-26 01:23:43 +0530

    boilerpalte code for mvc added

---
+++ src/main/java/io/github/Victor5H/entities/Address.java
@@ -0,0 +1,10 @@
+package io.github.Victor5H.entities;
+
+import javax.persistence.Entity;
+import javax.persistence.Table;
+
+@Entity(name = "address")
+@Table(name = "address")
+public class Address {
+
+}
---
+++ src/main/java/io/github/Victor5H/entities/Employee.java
@@ -0,0 +1,63 @@
+package io.github.Victor5H.entities;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity(name = "employee")
+@Table(name = "employee")
+public class Employee {
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	long id;
+	String name;
+	double salary;
+	Address address;
+
+	public long getId() {
+		return id;
+	}
+
+	public void setId(long id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public double getSalary() {
+		return salary;
+	}
+
+	public void setSalary(double salary) {
+		this.salary = salary;
+	}
+
+	public Address getAddress() {
+		return address;
+	}
+
+	public void setAddress(Address address) {
+		this.address = address;
+	}
+
+	public Employee(long id, String name, double salary, Address address) {
+		super();
+		this.id = id;
+		this.name = name;
+		this.salary = salary;
+		this.address = address;
+	}
+
+	public Employee() {
+		super();
+	}
+
+}
